982e5d7bf0be341727820a5dcc283e6d9512d6d0
Brian Craycraft <craycraftb24@hotmail.com>
2020-05-09 22:35:15 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,40 @@
 def using_include(array, element)
+  
+  array.include?(element)
 
 end
 
 def using_sort(array)
+  
+  expressions = ["I", "REALLY!?", "wow"]
+  expressions.sort
 
 end
 
 def using_reverse(array)
+  
+  expressions = ["wow", "REALLY!?", "Huh?", "arrays!"]
+  expressions.reverse
 
 end
 
 def using_first(array)
+  
+  expressions = ["wow", "REALLY!?", "Huh?"]
+  expressions.first
 
 end
 
 def using_last(array)
+  
+  expressions = ["Huh?", "REALLY!?", "wow", "arrays!"]
+  expressions.last
 
 end
 
 def using_size(array)
+  
+  expressions = ["Huh?", "REALLY!?", "wow", "arrays!", "Hooray!", "Yippee!"]
+  expressions.size
 
 end
