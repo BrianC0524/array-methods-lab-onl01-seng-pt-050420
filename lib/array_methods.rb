21b693dd1a6513b1d150440fdab7cda64f59e4c0
Brian Craycraft <craycraftb24@hotmail.com>
2020-05-09 22:29:14 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -35,6 +35,6 @@ end
 
 def using_size(array)
   
-  expressions = ["Huh?", "REALLY!?", "wow", "arrays!", "Horay"]
+  expressions = ["Huh?", "REALLY!?", "wow", "arrays!", "Hooray!", "Yippe!"]
 
 end
